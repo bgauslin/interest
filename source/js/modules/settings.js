aaf6b5342322a4c6dd3546aa662e9df51471210f
Ben Gauslin <ben@gauslin.com>
2018-07-30 07:41:26 -0400

    CURRENCIES moved into settings.js

--- source/js/modules/settings.js
+++ source/js/modules/settings.js
@@ -1,6 +1,5 @@
-import { CURRENCIES } from './currency';
 import { updateTotal } from'./userValues';
-import { MODES, THEMES } from './themes';
+import { THEMES } from './themes';
 
 
 /** @enum {Object} TODO: ... */
@@ -23,6 +22,46 @@ const SETTINGS = [
     fallback: 'usd',
   },
 ];
+
+/**
+ * @type {Object} Currency information for display and formatting.
+ */
+const CURRENCIES = {
+  label: 'Currency',
+  name: 'currency',
+  options: [
+    {
+      isoSymbol: 'USD',
+      label: '$',
+      value: 'usd',
+    },
+    {
+      isoSymbol: 'EUR',
+      label: '€',
+      value: 'eur',
+    },
+    {
+      isoSymbol: 'GBP',
+      label: '£',
+      value: 'gbp',
+    },
+    {
+      isoSymbol: 'YEN',
+      label: '¥',
+      value: 'yen',
+    },
+    {
+      isoSymbol: 'INR',
+      label: '₹',
+      value: 'inr',
+    },
+    // {
+    //   isoSymbol: 'WON',
+    //   label: '₩',
+    //   value: 'won',
+    // },
+  ],
+};
 
 /** @const {string} */
 const CHECKED_ATTR = 'checked';

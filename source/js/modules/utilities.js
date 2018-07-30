1e27de467668c27ad9ba70d5a3e33bbb6df00440
Ben Gauslin <ben@gauslin.com>
2018-07-30 07:00:53 -0400

    utilities.js formatting

--- source/js/modules/utilities.js
+++ source/js/modules/utilities.js
@@ -1,16 +1,16 @@
-/**
- * @description Removes 'no-js' attribute if JS is enabled.
- */
-const hasJs = () => {
-  document.body.removeAttribute('no-js');
-}
+/** @const {string} */
+const NO_JS_ATTR = 'no-js';
 
-/**
- * @description Removes 'no-touch' attribute if device is touch-enabled.
- */
+/** @const {string} */
+const NO_TOUCH_ATTR = 'no-touch';
+
+/** @description Removes 'no-js' attribute if JS is enabled. */
+const hasJs = () => document.body.removeAttribute(NO_JS_ATTR);
+
+/** @description Removes 'no-touch' attribute if device is touch-enabled. */
 const noTouch = () => {
   if ('ontouchstart' in window || navigator.msMaxTouchPoints > 0) {
-    document.body.removeAttribute('no-touch');
+    document.body.removeAttribute(NO_TOUCH_ATTR);
   }
 }
 

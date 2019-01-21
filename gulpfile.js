d058242da0788d57b0c8ba87aa786caf4bb26da0
Ben Gauslin <ben@gauslin.com>
2019-01-21 12:32:41 -0500

    babel update for async/await, repository url update

--- gulpfile.js
+++ gulpfile.js
@@ -41,7 +41,7 @@ gulp.task('icons', () => {
 // Compile and uglify JavaScript.
 gulp.task('js', () => {
   return browserify({ entries: pkg.paths.js.src, debug: true })
-    .transform('babelify', { presets: ['@babel/preset-env'] })
+    .transform('babelify', {'presets': [['@babel/preset-env', {'targets': {'node': 'current'}}]]})
     .bundle()
     .pipe(source(pkg.paths.js.bundleSrc))
     .pipe(buffer())

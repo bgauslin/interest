7c6349e85819d9774542ffb7af70db87dd150b2d
Ben Gauslin <ben@gauslin.com>
2019-07-08 21:03:28 -0400

    uglifyjs-webpack-plugin removed

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,15 +1,11 @@
 const common = require('./webpack.common.js');
 const merge = require('webpack-merge');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
   output: {
     filename: 'interest.[contenthash].js',
-  },
-  optimization: {
-    minimizer: [new UglifyJsPlugin()],
   },
   plugins: [
     new HtmlWebpackPlugin({

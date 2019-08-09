3285709631e71f653dfb236cb6e41e17e038cd1d
Ben Gauslin <ben@gauslin.com>
2019-08-09 05:49:00 -0400

    updates rendering methods in App

--- src/js/modules/App.ts
+++ src/js/modules/App.ts
@@ -10,22 +10,20 @@ enum FooterInfo {
 };
 
 class App {
-  appEl_: Element;
+  appEl_: HTMLElement;
 
   constructor(selector: string) {
     this.appEl_ = document.querySelector(selector);
   }
 
   /**
-   * Renders all HTML for the app.
+   * Registers custom elements and renders all HTML for the app.
    */
   public init(): void {
-    // Register custom elements
     customElements.define('my-expandable', Expandable);
     customElements.define('user-settings', UserSettings);
     customElements.define('user-values', UserValues);
-    
-    // Render everything into the DOM.
+
     this.renderHeader_();
     this.renderContent_();
     this.renderFooter_();
@@ -35,45 +33,48 @@ class App {
    * Renders header element into the DOM.
    */
   private renderHeader_(): void {
-    this.appEl_.innerHTML += `
-      <header class="header">
-        <div class="header__frame">
-          <h1 class="header__title">${document.title}</h1>
-          <user-settings class="settings" id="settings"></user-settings>
-        </div>
-      </header>
+    const html = `\
+      <header class="header">\
+        <div class="header__frame">\
+          <h1 class="header__title">${document.title}</h1>\
+          <user-settings class="settings" id="settings"></user-settings>\
+        </div>\
+      </header>\
     `;
+    this.appEl_.innerHTML += html.replace(/\s\s/g, '');
   }
 
   /**
    * Renders user inputs, table, expandable, and click mask into the DOM.
    */
   private renderContent_(): void {
-    this.appEl_.innerHTML += `
-      <user-values class="values"></user-values>
-      ${this.renderTable_('table')}
-      <my-expandable class="expandable" target="table" label="table"></my-expandable>
+    const html = `
+      <user-values class="values"></user-values>\
+      ${this.renderTable_('table')}\
+      <my-expandable class="expandable" target="table" label="table"></my-expandable>\
     `;
+    this.appEl_.innerHTML += html.replace(/\s\s/g, '');
   }
 
   /**
    * Returns rendered table element for displaying calculated user values.
    */
   private renderTable_(classname: string, id: string = classname): string {
-    return `
-      <div class="${classname}" id="${id}">
-        <div class="${classname}__frame">
-          <table class="${classname}__data"></table>
-        </div>
-        <p class="rotate-screen">
-          Rotate screen to view <span>Interest</span> and <span>Growth</span> columns.
-        </p>
-      </div>
+    const html = `\
+      <div class="${classname}" id="${id}">\
+        <div class="${classname}__frame">\
+          <table class="${classname}__data"></table>\
+        </div>\
+        <p class="rotate-screen">\
+          Rotate screen to view <span>Interest</span> and <span>Growth</span> columns.\
+        </p>\
+      </div>\
     `;
+    return html.replace(/\s\s/g, '');
   }
 
   /**
-   * Renders footer element with copyright info and a link into the DOM.
+   * Renders footer element into the DOM.
    */
   private renderFooter_(): void {
     const { label, title, url, yearStart } = FooterInfo;

637c265741bf3d7fef3180522eb54868785c163f
Ben Gauslin <ben@gauslin.com>
2020-02-17 17:33:03 -0500

    Terser plugin wired up for slightly smaller JS

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,24 @@
 const common = require('./webpack.common.js');
 const merge = require('webpack-merge');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false,
+        terserOptions: {
+          mangle: true,
+          output: {
+            comments: false,
+          },
+        }
+      })
+    ],
+  },
   output: {
     filename: '[name].[contenthash].js',
   },
@@ -11,5 +26,5 @@ module.exports = merge(common, {
     new MiniCssExtractPlugin({
       filename: '[name].[contenthash].css',
     }),
-  ]
+  ],
 });

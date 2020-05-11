bf77ddae7e7baa2c674b1ae1ff7b2c9b9fd53600
Ben Gauslin <bgauslin@gmail.com>
2020-05-11 07:42:43 -0400

    Adds ARIA attributes to settings toggle and menu; tweaks its layout slightly

--- src/js/custom_elements/UserSettings.ts
+++ src/js/custom_elements/UserSettings.ts
@@ -9,7 +9,10 @@ interface SettingsOptions {
   value: string,
 }
 
+const ARIA_EXPANDED_ATTR: string = 'aria-expanded';
+const ARIA_HIDDEN_ATTR: string = 'aria-hidden';
 const CURRENCY_ATTR: string = 'currency';
+const MENU_CLASS: string = 'menu';
 const OPEN_ATTR: string = 'open';
 const THEME_ATTR: string = 'theme';
 const TOGGLE_CLASS: string = 'settings__toggle';
@@ -42,6 +45,7 @@ const Settings: SettingsFields[] = [
  */
 class UserSettings extends HTMLElement {
   private closeMenuListener_: any;
+  private menu_: HTMLElement;
   private toggleButton_: HTMLButtonElement;
 
   constructor() {
@@ -83,6 +87,8 @@ class UserSettings extends HTMLElement {
         this.closeMenu_();
       } else {
         this.setAttribute(OPEN_ATTR, '');
+        this.toggleButton_.setAttribute(ARIA_EXPANDED_ATTR, 'true');
+        this.menu_.setAttribute(ARIA_HIDDEN_ATTR, 'false');
         window.requestAnimationFrame(() => {
           document.addEventListener('click', this.closeMenuListener_);
         });
@@ -128,6 +134,8 @@ class UserSettings extends HTMLElement {
   private closeMenu_(): void {
     this.removeAttribute(OPEN_ATTR);
     document.removeEventListener('click', this.closeMenuListener_);
+    this.toggleButton_.setAttribute(ARIA_EXPANDED_ATTR, 'false');
+    this.menu_.setAttribute(ARIA_HIDDEN_ATTR, 'true');
   }
 
   /**
@@ -191,13 +199,23 @@ class UserSettings extends HTMLElement {
       `;
     });
 
+    const label = 'Settings';
     const html = `\
-      <button class="${TOGGLE_CLASS}" tabindex="0" aria-label="Settings">\
-        <svg class="settings__icon" viewbox="0 0 24 24">\
+      <button class="${TOGGLE_CLASS}" \
+        tabindex="0" \
+        id="menu-toggle" \
+        aria-controls="menu" \
+        title="${label}" \
+        aria-label="${label}" \
+        ${ARIA_EXPANDED_ATTR}="false">\
+        <svg class="settings__icon" viewbox="0 0 24 24" aria-hidden="true">\
           <path d="M22.847 14.798 L20.85 13.645 C21.052 12.558 21.052 11.442 20.85 10.355 L22.847 9.202 C23.077 9.07 23.18 8.798 23.105 8.545 22.584 6.877 21.698 5.367 20.541 4.111 20.362 3.919 20.072 3.872 19.847 4.003 L17.85 5.156 C17.011 4.434 16.045 3.877 15 3.511 L15 1.209 C15 0.947 14.817 0.717 14.559 0.661 12.839 0.277 11.077 0.295 9.441 0.661 9.183 0.717 9 0.947 9 1.209 L9 3.516 C7.959 3.886 6.994 4.444 6.15 5.161 L4.158 4.008 C3.928 3.877 3.642 3.919 3.464 4.116 2.306 5.367 1.42 6.877 0.9 8.55 0.82 8.803 0.928 9.075 1.158 9.206 L3.155 10.359 C2.953 11.447 2.953 12.563 3.155 13.65 L1.158 14.803 C0.928 14.934 0.825 15.206 0.9 15.459 1.42 17.128 2.306 18.638 3.464 19.894 3.642 20.086 3.933 20.133 4.158 20.002 L6.155 18.849 C6.994 19.57 7.959 20.128 9.005 20.494 L9.005 22.8 C9.005 23.063 9.188 23.292 9.445 23.349 11.166 23.733 12.928 23.714 14.564 23.349 14.822 23.292 15.005 23.063 15.005 22.8 L15.005 20.494 C16.045 20.124 17.011 19.566 17.855 18.849 L19.851 20.002 C20.081 20.133 20.367 20.091 20.545 19.894 21.703 18.642 22.589 17.133 23.109 15.459 23.18 15.202 23.077 14.93 22.847 14.798 Z M12 15.75 C9.933 15.75 8.25 14.067 8.25 12 8.25 9.933 9.933 8.25 12 8.25 14.067 8.25 15.75 9.933 15.75 12 15.75 14.067 14.067 15.75 12 15.75 Z"/>\
         </svg>\
       </button>\
-      <div class="menu">\
+      <div class="${MENU_CLASS}" \
+        id="menu" \
+        aria-controlledby="menu-toggle" \
+        aria-hidden="true">\
         <div class="menu__content">\
           ${menuGroups}\
         </div>\
@@ -205,7 +223,9 @@ class UserSettings extends HTMLElement {
     `;
 
     this.innerHTML = html.replace(/\s\s/g, '');
+
     this.toggleButton_ = this.querySelector(`.${TOGGLE_CLASS}`);
+    this.menu_ = this.querySelector(`.${MENU_CLASS}`);
   }
 }
 

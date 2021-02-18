b9d53cbe5cecdf643103783ca689a5bac10b4ec6
Ben Gauslin <bgauslin@gmail.com>
2021-02-18 17:54:43 -0500

    blurry fixed header replaces shifty-header; adds header-background css var

--- src/js/interest.ts
+++ src/js/interest.ts
@@ -2,7 +2,6 @@ require('dotenv').config();
 
 import './custom_elements/expandable/expandable';
 import './custom_elements/settings/settings';
-import './custom_elements/shifty_header/shifty_header';
 import './custom_elements/tools/tools';
 import './custom_elements/user_values/user_values';
 import './custom_elements/visibility_toggle/visibility_toggle';

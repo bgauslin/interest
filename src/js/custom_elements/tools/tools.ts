f422a21e606c7198bc7c1ab9d9475a7f4db571b8
Ben Gauslin <github@gauslin.com>
2022-09-26 06:25:10 -0500

    Adds new analytics

--- src/js/custom_elements/tools/tools.ts
+++ src/js/custom_elements/tools/tools.ts
@@ -14,6 +14,7 @@ export class Tools extends HTMLElement {
     if (!this.hasSetup) {
       this.setupDom();
       this.touchEnabled();
+      this.analytics();
       this.hasSetup = true;
     }
   }
@@ -23,7 +24,19 @@ export class Tools extends HTMLElement {
    */
   private setupDom() {
     document.body.removeAttribute('no-js');
-    document.querySelector('noscript')?.remove();
+    document.querySelector('noscript').remove();
+  }
+
+  /**
+   * Initializes analytics.
+   */
+  private analytics() {
+    if (process.env.NODE_ENV === 'production') {
+      window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=document.createElement("script");r.type="text/javascript",r.async=!0,r.src="https://cdn.heapanalytics.com/js/heap-"+e+".js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(r,a);for(var n=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],o=0;o<p.length;o++)heap[p[o]]=n(p[o])};
+      heap.load('1593720077');
+
+      document.body.innerHTML += '<script src="//static.getclicky.com/101381864.js" async></script>';
+    }
   }
 
   /**

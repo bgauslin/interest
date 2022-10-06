3ee037eda59a6beb9f3ba8fc791c88f770ebc9dc
Ben Gauslin <github@gauslin.com>
2022-10-06 09:30:18 -0500

    unneeded custom element and attributes deleted

--- src/js/interest.ts
+++ src/js/interest.ts
@@ -1,17 +1,6 @@
-require('dotenv').config();
+import '../styles/index';
 
 import './custom_elements/expandable/expandable';
 import './custom_elements/settings/settings';
-import './custom_elements/tools/tools';
 import './custom_elements/user_values/user_values';
 import './custom_elements/visibility_toggle/visibility_toggle';
-
-import '../stylus/index.styl';
-
-if (process.env.NODE_ENV === 'production') {
-  if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/sw.js');
-    });
-  }
-}

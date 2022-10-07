90168b1f3cfaf31494ab661ed51a2257c924444f
Ben Gauslin <github@gauslin.com>
2022-10-07 08:44:18 -0500

    minor renaming in const.scss

--- src/js/app.ts
+++ src/js/app.ts
@@ -1,8 +1,8 @@
-// App stylesheet. (Required in main JS entry file for Webpack.)
+// App stylesheet (required in JS entry file for Webpack).
 import '../styles/index';
 
 // Web components.
-// import './components/expandable/expandable';
+import './components/expandable/expandable';
 import './components/settings/settings';
 import './components/table/table';
 import './components/user-values/user-values';

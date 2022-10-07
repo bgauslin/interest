dbe4802d368cb2b6c4ae2095e0616dbff13d3133
Ben Gauslin <github@gauslin.com>
2022-10-07 07:17:38 -0500

    refactors/renames html and js entry files

---
+++ src/js/app.ts
@@ -0,0 +1,9 @@
+// App stylesheet. (Required in main JS entry file for Webpack.)
+import '../styles/index';
+
+// Web components.
+// import './components/expandable/expandable';
+import './components/settings/settings';
+import './components/table/table';
+import './components/user-values/user-values';
+
--- src/js/interest.ts
+++
@@ -1,6 +0,0 @@
-import '../styles/index';
-
-import './custom_elements/expandable/expandable';
-import './custom_elements/settings/settings';
-import './custom_elements/user_values/user_values';
-import './custom_elements/visibility_toggle/visibility_toggle';

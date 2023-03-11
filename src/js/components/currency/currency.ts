71014c317ece95b8576cfceba64b911477030449
Ben Gauslin <github@gauslin.com>
2023-03-11 11:33:55 -0600

    moves widget to upper corner
    icon button, flip removed, simpler styles

--- src/js/components/currency/currency.ts
+++ src/js/components/currency/currency.ts
@@ -20,7 +20,6 @@ class Currency extends LitElement {
   @state() currency = DEFAULT_CURRENCY;
   @state() currencyEvent = 'updateCurrency';
   @state() currencyListener: EventListenerObject;
-  @state() flip: Boolean = false;
   @state() keyListener: EventListenerObject;
   @state() open: Boolean = false;
 
@@ -68,9 +67,6 @@ class Currency extends LitElement {
   }
 
   private openMenu() {
-    const viewportHeight = window.innerHeight;
-    const {height, y} = this.form.getBoundingClientRect();
-    this.flip = (y + height > viewportHeight);
     this.dialog.show();
     this.checked.focus();
   }
@@ -80,7 +76,6 @@ class Currency extends LitElement {
     this.dialog.addEventListener('transitionend', () => {
       this.dialog.close();
       this.closing = false;
-      this.flip = false;
     }, {once: true});
   }
 
@@ -114,8 +109,14 @@ class Currency extends LitElement {
         aria-controls="menu"
         aria-expanded="${this.open}"
         aria-haspopup="true"
+        aria-label="${label}"
+        title="${label}"
         type="button"
-        @click="${this.toggleMenu}">${label}</button>
+        @click="${this.toggleMenu}">
+          <svg aria-hidden="true" viewbox="0 0 24 24">
+            <path d="M19.14,12.94c0.04-0.3,0.06-0.61,0.06-0.94c0-0.32-0.02-0.64-0.07-0.94l2.03-1.58c0.18-0.14,0.23-0.41,0.12-0.61 l-1.92-3.32c-0.12-0.22-0.37-0.29-0.59-0.22l-2.39,0.96c-0.5-0.38-1.03-0.7-1.62-0.94L14.4,2.81c-0.04-0.24-0.24-0.41-0.48-0.41 h-3.84c-0.24,0-0.43,0.17-0.47,0.41L9.25,5.35C8.66,5.59,8.12,5.92,7.63,6.29L5.24,5.33c-0.22-0.08-0.47,0-0.59,0.22L2.74,8.87 C2.62,9.08,2.66,9.34,2.86,9.48l2.03,1.58C4.84,11.36,4.8,11.69,4.8,12s0.02,0.64,0.07,0.94l-2.03,1.58 c-0.18,0.14-0.23,0.41-0.12,0.61l1.92,3.32c0.12,0.22,0.37,0.29,0.59,0.22l2.39-0.96c0.5,0.38,1.03,0.7,1.62,0.94l0.36,2.54 c0.05,0.24,0.24,0.41,0.48,0.41h3.84c0.24,0,0.44-0.17,0.47-0.41l0.36-2.54c0.59-0.24,1.13-0.56,1.62-0.94l2.39,0.96 c0.22,0.08,0.47,0,0.59-0.22l1.92-3.32c0.12-0.22,0.07-0.47-0.12-0.61L19.14,12.94z M12,15.6c-1.98,0-3.6-1.62-3.6-3.6 s1.62-3.6,3.6-3.6s3.6,1.62,3.6,3.6S13.98,15.6,12,15.6z"/>
+          </svg>
+        </button>
     `;
   }
 
@@ -123,7 +124,6 @@ class Currency extends LitElement {
     return html`
       <dialog
         ?data-closing="${this.closing}"
-        ?data-flip="${this.flip}"
         id="menu">
         <form @change="${this.getCurrency}">
           <ul>

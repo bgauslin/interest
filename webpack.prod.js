e47ab0e6d14a7987ea18c4bf4d744fd73b2a2e2a
Ben Gauslin <github@gauslin.com>
2025-10-22 13:41:49 -0300

    adds base path (no-op)

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,7 @@
 const common = require('./webpack.common.js');
 const {merge} = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-// const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
@@ -9,6 +9,11 @@ module.exports = merge(common, {
     filename: '[contenthash].js',
   },
   plugins: [
+    new HtmlWebpackPlugin({
+      base: '/interest/',
+      filename: 'index.html',
+      template: 'src/html/index.html',
+    }),
     new MiniCssExtractPlugin({
       filename: '[contenthash].css',
     }),
